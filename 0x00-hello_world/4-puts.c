a215961ce8e1dc8ecaf092ca1fc3b89d1103e013
jameslarbie <jameslarbie30@gmail.com>
2022-07-01 14:44:38 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,22 @@
 #!/bin/bash
 #include <stdio.h>
-/
-*main - Entry point
-*Return: Always 0 (success)
-* /
-int main()
-{	
-printf("\"Programming is like building a multilingual puzzle");
-return (0);
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+
 }
