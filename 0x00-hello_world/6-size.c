5a58f1ada0c024c8a94e96a2f844a32d7972dee8
jameslarbie <jameslarbie30@gmail.com>
2022-07-02 11:21:14 -0700

    hot fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,19 @@
 /**
  * main-Entry Point
  * Return: Always 0 (success)
- */
+*/
 int main(void)
 {
-int i;
-long int I;
-long long int II;
-float d;
-char c;
+	int i;
+	long int I;
+	long long int II;
+	float d;
+	char c;
 
-printf("Size of a char:%lu byte(s)\n", sizeof(char));
-printf("Size of an int:%lu byte(s)\n", sizeof(int));
-printf("Size of a long long int:%lu byte(s)\n", sizeof(long int));
-printf("Size of a float:%lu byte(s)\n", sizeof(long long int));
-printf("Size of a float:%lu byte(s)\n", sizeof(float));
-
-return (0);
+	printf("Size of a char:%zu byte(s)\n", sizeof(char));
+	printf("Size of an int:%zu byte(s)\n", sizeof(int));
+	printf("Size of a long long int:%zu byte(s)\n", sizeof(long int));
+	printf("Size of a float:%zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%zu byte(s)\n", sizeof(float));
+	return (0);
 }
