68c14beb60673f875200a2f7cc8dd27aefd9ab8a
jameslarbie <jameslarbie30@gmail.com>
2022-07-05 15:56:13 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 -> prints the lower case alphabets
+*/
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
