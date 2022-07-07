665d9275c364ae3455aa6bf6754e8b4a97823366
jameslarbie <jameslarbie30@gmail.com>
2022-07-07 02:42:29 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,17 @@
 
 /**
  * _islower -> checks if the character is lower case
+ * @c: character to be checked
  * Return: returns 1 and 0 depending on the condition
  */
 int _islower(int c)
 {
+	int lower = (c >= 'a' && c <= 'z');
+	int upper = (c >= 'A' && c <= 'Z');
 
-	return (c >= 'a' && c <= 'z');
+	if (lower || upper)
+		return (1);
+	else
+		return (0);
 }
 
